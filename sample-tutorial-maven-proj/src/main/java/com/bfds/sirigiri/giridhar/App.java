b3d79ee1c68cbd467207c4963adc3b08659085af
sirigiri <dt82650@.ad.dstsystems.com>
2013-04-23 18:43:22 +0530

    Test Release Project

---
+++ sample-tutorial-maven-proj/src/main/java/com/bfds/sirigiri/giridhar/App.java
@@ -0,0 +1,13 @@
+package com.bfds.sirigiri.giridhar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
